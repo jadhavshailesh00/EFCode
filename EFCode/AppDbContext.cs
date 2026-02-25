a271957ceb0512aa0c840dc0fc5659e35ab72c83
shailesh jadhav <jadhavshailesh00@gmail.com>
2026-02-25 08:24:32 +0530

    Added Code First approach

---
+++ EFCode/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EFCode.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace EFCode
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> student { get; set; } 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+        "Server=sqlserver;Database=EFCoreDemo;User Id=sa;Password=Your_password123;TrustServerCertificate=True;");
+
+        }
+    }
+}
---
+++ EFCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using EFCode.Model;
+using EFCode;
+using Microsoft.EntityFrameworkCore;
+
+
+using (var context = new AppDbContext())
+{
+    context.Database.Migrate();
+}
+
+using (var context = new AppDbContext())
+{
+    // INSERT
+    context.student.Add(new Student { Name = "John", Age = 22 });
+    context.SaveChanges();
+
+    // SELECT
+    var students = context.student.ToList();
+
+    // UPDATE
+    var student = context.student.First();
+    student.Age = 25;
+    context.SaveChanges();
+
+    // DELETE
+    context.student.Remove(student);
+    context.SaveChanges();
+}
